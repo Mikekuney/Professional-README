5d73b8b7ce601e9d7b5b0fdb3cdf5db0950d272b
Mikekuney <96913841+Mikekuney@users.noreply.github.com>
2022-04-20 18:38:03 -0400

    added initialize function

--- Develop/index.js
+++ Develop/index.js
@@ -123,7 +123,7 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
 const getUserInfo = () => {
     inquirer.prompt(questions).then((answers) => {
         fs.writeFile("./utils/README.md", generateData({ ...answers}), (err) => {
@@ -133,7 +133,9 @@ const getUserInfo = () => {
 };
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    getUserInfo();
+}
 
 // Function call to initialize app
 init();
